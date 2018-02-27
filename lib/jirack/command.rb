6ea6f507d7ade6dcee5db0de3f21f51768f805fd
rhythm191 <sac2ndg+develop@gmail.com>
2018-02-27 18:29:03 +0900

    add unassign list

--- lib/jirack/command.rb
+++ lib/jirack/command.rb
@@ -35,26 +35,32 @@ module Jirack
     end
 
     desc 'list', 'show your active issue'
-    method_option 'sum-point',  type: :boolean, desc: 'show your all issue point'
+    method_option 'unassign', type: :boolean, aliases: '-u', desc: 'show your all issue point'
+    method_option 'sum-point', type: :boolean, aliases: '-n', desc: 'show your all issue point'
     def list
       cred = Jirack::Credential.new
       client = cred.jira_client
 
       active_sprint =  active_sprint(client, project_board(client, cred.project_name)['id'].to_i)
 
-      if options.key?('sum-point')
-        sum_points = active_sprint_issue(client, cred.project_name, active_sprint['name']).inject(0.0) {|sum, issue| sum + issue.points }
+      if options.key?('unassign')
+        puts "#{ active_sprint['name'] } unassign issues: "
+        active_unassign_issue(client, cred.project_name, active_sprint['name']).each do |issue|
+          puts "#{ issue.key }: #{ issue.summary }(#{issue.id}) [#{ issue.points }] #{ issue.status.name } "
+        end
+      elsif options.key?('sum-point')
+        sum_points = active_assign_issue(client, cred.project_name, active_sprint['name']).inject(0.0) {|sum, issue| sum + issue.points }
         puts "#{ active_sprint['name'] } points: #{ sum_points }"
       else
         puts "#{ active_sprint['name'] } issues: "
-        active_sprint_issue(client, cred.project_name, active_sprint['name']).each do |issue|
+        active_assign_issue(client, cred.project_name, active_sprint['name']).each do |issue|
           puts "#{ issue.key }: #{ issue.summary }(#{issue.id}) [#{ issue.points }] #{ issue.status.name } "
         end
       end
     end
 
     desc 'forward issue_number', 'forward issue status'
-    method_option 'message',  :aliases => '-m', desc: 'notify slack message'
+    method_option 'message',  aliases: '-m', desc: 'notify slack message'
     def forward(issue_number)
       cred = Jirack::Credential.new
       client = cred.jira_client
@@ -138,8 +144,12 @@ module Jirack
       client.Agile.get_sprints(board_id, state: 'active')['values'].first
     end
 
-    def active_sprint_issue(client, project_name, sprint_name)
+    def active_assign_issue(client, project_name, sprint_name)
       JIRA::Resource::Issue.jql(client, "project=\"#{ project_name }\" AND assignee = currentuser() AND cf[10007] = \"#{ sprint_name }\"")
+    end
+
+    def active_unassign_issue(client, project_name, sprint_name)
+      JIRA::Resource::Issue.jql(client, "project=\"#{ project_name }\" AND assignee = NULL AND cf[10007] = \"#{ sprint_name }\"")
     end
 
     def issue_url(issue)
